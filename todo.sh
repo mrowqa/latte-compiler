4108dce8452cc351bbdf7238310e3a28736e6918
Mrowqa <artur.jamro@gmail.com>
2018-12-10 15:43:22 +0100

    Improved todo.sh

--- todo.sh
+++ todo.sh
@@ -1,1 +1,6 @@
-grep -riE "todo|fixme" . --include "*.rs" --include "*.lalrpop"
+if [[ "$1" == "--opt" ]]; then
+    grep -riE "todo|fixme" . --include "*.rs" --include "*.lalrpop"
+else
+    grep -riE "todo|fixme" . --include "*.rs" --include "*.lalrpop" | grep -v "\(optional\)"
+fi
+
