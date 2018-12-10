c5819d8a1ee70ebf3974540b6a303fec25065242
Mrowqa <artur.jamro@gmail.com>
2018-12-10 17:31:37 +0100

    main.rs now reads actual files

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
 extern crate latte_compiler;
 
 use latte_compiler::compile;
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::process;
 
 fn main() {
-    // todo run on actual file
-    let res = compile(
-        "input",
-        r#"
-int main(int arg) {
-    return 0;
-}
+    let args: Vec<_> = env::args().collect();
 
-class A {
-    int a;
-
-    int foo(int b) {
-        return a+b+c;  // c not defined
+    if args.len() != 2 {
+        eprintln!("Usage: {} <filename.lat>", args[0]);
+        process::exit(1);
     }
-}
 
-"#,
-    );
+    let input_file_str = &args[1];
+    let input_file = Path::new(&input_file_str);
+    let code = match fs::read_to_string(input_file) {
+        Ok(s) => s,
+        Err(_) => {
+            eprintln!("Cannot read file: {}", input_file.display());
+            process::exit(1);
+        }
+    };
+
+    let res = compile(input_file_str, &code);
     match res {
         Ok(prog) => println!("{:?}", prog),
-        Err(msg) => println!("{}", msg),
+        Err(msg) => {
+            println!("{}", msg);
+            process::exit(1);
+        }
     }
 }
