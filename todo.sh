5462e82a6d5be3a70ef2e150926c6da246e72fb8
Artur Jamro <artur.jamro@gmail.com>
2018-12-31 17:49:54 +0100

    Improved todo.sh

--- todo.sh
+++ todo.sh
@@ -1,5 +1,7 @@
 if [[ "$1" == "--opt" ]]; then
     grep -riE "todo|fixme" . --include "*.rs" --include "*.lalrpop"
+elif [[ "$1" == "--no-ext" ]]; then
+    grep -riE "todo|fixme" . --include "*.rs" --include "*.lalrpop" | grep -v "\(optional\)" | grep -v "\(ext\)"
 else
     grep -riE "todo|fixme" . --include "*.rs" --include "*.lalrpop" | grep -v "\(optional\)"
 fi
